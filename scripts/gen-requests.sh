178d36b1f7a419023ce3eb895c0b25e5573a7572
marwyp <marekwypich@gmail.com>
2025-01-04 16:26:40 +0100

    wget at the same time to /dev/null

--- scripts/gen-requests.sh
+++ scripts/gen-requests.sh
@@ -1,10 +1,10 @@
 #!/bi$n/bash
 SERVER=$1
-MINWAIT=0
-MAXWAIT=5
+MINWAIT=3
+MAXWAIT=7
 while :
 do
-    wget "http://${SERVER}:8080/5MBfile"  > /dev/null
+    wget "http://${SERVER}:8080/5MBfile" -O /dev/null &
     sleep $((MINWAIT+RANDOM % (MAXWAIT-MINWAIT)))
 done
 
